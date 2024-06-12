f3c8e361713e303d5e83c65af2449b823f917914
pearlyn-r <91005236+pearlyn-r@users.noreply.github.com>
2024-06-12 16:55:25 +0530

    Delete rag directory

--- rag/constants.py
+++
@@ -1,9 +0,0 @@
-# Constants
-BASE_URL = 'http://localhost:11434/v1'
-API_KEY = 'llama3'
-OLLAMA_MODEL = 'llama3'
-
-SYSTEM_MESSAGE = """You are a helpful assistant and your primary role is that of a financial advisor to learn about the user's financial profile 
-and preferences and bring in extra relevant information to the user query from outside the given context. Extract relevant information from the text provided to
-ultimately help the user make a decision on what to invest by making normal conversation with them. Your role is to have a natural conversation with the user, understand their financial situation and preferences,
-and provide tailored advice and suggestions for potential investments or financial strategies."""
--- rag/whispermodel.py
+++
@@ -1,4 +0,0 @@
-import whisper
-
-def initialize_whisper_model():
-    return whisper.load_model("base")
