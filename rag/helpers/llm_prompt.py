56a2e82767d3389205ba4e80fa8ab0b38f51a70c
pearlyn-r <91005236+pearlyn-r@users.noreply.github.com>
2024-06-19 14:21:42 +0530

    Update llm_prompt.py
    fixed bug

--- rag/helpers/llm_prompt.py
+++ rag/helpers/llm_prompt.py
@@ -2,14 +2,15 @@
 import streamlit as st
 def ollama_chat(input, system_message, vault_content, ollama_model, conversation_history, client):
     print("Called: ollama_chat")
+    
     context_str = "\n\n".join(vault_content) if vault_content else "No relevant context found."
+    
     # Combine user input with context for the model
     #user_input_with_context = input + "\n\nRelevant Context:\n" + context_str
     # Append the plain user input to the conversation history
-    conversation_history.append({"role": "user", "content": input})
+    #conversation_history.append({"role": "user", "content": input})
 
-    # Check for consecutive duplicate user inputs for the last two entries
-   
+    # Check for consecutive duplicate user inputs for the last two entries   
     # Prepare messages with system message and conversation history (excluding context)
     messages = [{"role": "system", "content": system_message}, *conversation_history]
     messages.append({"role": "user", "content": context_str})
@@ -23,7 +24,5 @@ def ollama_chat(input, system_message, vault_content, ollama_model, conversation
     
     # Extract and append the assistant response to the conversation history
     assistant_response = response.choices[0].message.content
-    conversation_history.append({"role": "assistant", "content": assistant_response})
-
-
+    #conversation_history.append({"role": "assistant", "content": assistant_response})
     return assistant_response
