82dbfe213b896ef1ab6ada4f7e3750677ec78bc2
pearlyn-r <91005236+pearlyn-r@users.noreply.github.com>
2024-06-19 15:23:02 +0530

    Update llm_prompt.py
    bug fixed

--- rag_qdrant/helpers/llm_prompt.py
+++ rag_qdrant/helpers/llm_prompt.py
@@ -13,7 +13,7 @@ def ollama_chat(input, system_message, ollama_model, conversation_history, clien
     context_str = "relevant context is \n\n".join(result.payload['text'] for result in search_result) if search_result else "No relevant context found."
     
     #user_input_with_context = input + "\n\nRelevant Context:\n" + context_str 
-    conversation_history.append({"role": "user", "content": input})
+    #conversation_history.append({"role": "user", "content": input})
     
     messages = [{"role": "system", "content": system_message}, *conversation_history]
     messages.append({"role": "user", "content": context_str})
@@ -23,5 +23,6 @@ def ollama_chat(input, system_message, ollama_model, conversation_history, clien
         max_tokens=2000,
     )
     assistant_response = response.choices[0].message.content
-    conversation_history.append({"role": "assistant", "content": assistant_response})
+    #conversation_history.append({"role": "assistant", "content": assistant_response})
+    
     return assistant_response
