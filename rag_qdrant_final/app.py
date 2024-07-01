b079d6f4a23977a9e914fbdc5f168e881e55f5d1
pearlyn-r <91005236+pearlyn-r@users.noreply.github.com>
2024-07-01 17:10:29 +0530

    Add files via upload

---
+++ rag_qdrant_final/app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+from audio_recorder_streamlit import audio_recorder
+from streamlit_float import float_init
+from config.constants import BASE_URL, API_KEY, OLLAMA_MODEL
+from audio.whispermodel import initialize_whisper_model
+from helpers.ollama_client import initialize_ollama_client
+from helpers.embeddings import  generate_vault_embeddings
+from helpers.llm_prompt import ollama_chat
+from audio.audio_input import handle_audio_input
+from config.session_state import initialize_session_state
+
+float_init()
+initialize_session_state()
+whisper_model = initialize_whisper_model()
+client = initialize_ollama_client(BASE_URL, API_KEY)
+#unhash the bottom code if it is ur first time running it.
+#generate_vault_embeddings("data/vault.txt", OLLAMA_MODEL, "demo")
+
+def display_conversation_history():
+    for msg in st.session_state.conversation_history:
+        with st.chat_message(msg["role"]):
+            st.markdown(msg["content"])
+
+
+st.title("Mutual Fund Advisor with Llama3")
+footer_container = st.container()
+with footer_container:
+    audio_bytes = audio_recorder()
+    # Reset audio_processed flag if new audio is recorded
+    if audio_bytes:
+        st.session_state.audio_processed = False
+
+display_conversation_history()
+
+if audio_bytes and not st.session_state.audio_processed:
+    transcript = handle_audio_input(audio_bytes, whisper_model)
+    if transcript:
+        # st.write(f"**Transcribed Text:** {transcript}")
+
+        with st.chat_message("user"):
+            st.markdown(transcript)
+        # Store user input in conversation history
+        st.session_state.conversation_history.append({"role": "user", "content": transcript})
+
+        response = ollama_chat(transcript, st.session_state.system_message, OLLAMA_MODEL, st.session_state.conversation_history, client)
+        # st.write(f"**Assistant:** {response}")
+        with st.chat_message("assistant"):
+            st.markdown(response)
+
+        # Store assistant response in conversation history
+        st.session_state.conversation_history.append({"role": "assistant", "content": response})
+
+        st.session_state.audio_processed = True
+        
+
+    footer_container.float("bottom: 0rem;")
+
+footer_container.write("")
---
+++ rag_qdrant_final/config/constants.py
@@ -0,0 +1,45 @@
+# Constants
+BASE_URL = 'http://localhost:11434/v1'
+API_KEY = 'llama3'
+OLLAMA_MODEL = 'llama3'
+SYSTEM_MESSAGE = """You are a helpful assistant acting as a financial advisor. 
+Your first task is to ask the user 4 specific questions to create a risk profile. Ask these questions one at a time:
+
+1. What do you normally associate with the word "risk"?
+   - Dangerous
+   - Uncertainty
+   - Opportunity
+   - Thrill
+
+2. Your friend suggested an investment option that has gained popularity due to returns. Your reaction is to:
+   - Immediately invest a large amount
+   - Immediately invest a tracking amount
+   - Keep it on my watch list for later
+   - Pull up my friend for indulging in speculation
+
+3. Please choose a time horizon within which you might need to liquidate the majority of your investments.
+   - <1 year
+   - 1-3 years
+   - 3-5 years
+   - >5 years
+
+4. What are your objectives/expectations from investments?
+   - Preservation of capital
+   - Returns above deposit rates
+   - Growth that beats the index tracker
+   - Maximise returns with suitable products
+
+When asking the questions:
+- If the user's response is relevant and matches one of the options, say "Next question" before moving to the next question.
+- If the user's response is irrelevant or unclear, repeat the same question and ask for clarification.
+- After all questions are answered, start your analysis with "Based on your answers".
+
+After asking all questions and receiving responses:
+- Analyze the user's answers to understand their risk tolerance, investment horizon, and financial goals.
+- Use the provided relevant context to recommend specific mutual funds that match the user's risk profile and goals.
+- Provide a tailored recommendation for two investment options based on their profile and the list of mutual funds that are passed to you.
+- Use simple language and terminology understandable by someone who isn't educated about mutual funds.
+- Explain the rationale behind your recommendation, referencing specific details from the provided context.
+
+Then continue with normal conversation, providing financial advice based on the user's risk profile and the available mutual fund data.
+"""
